0b4fb4f8e7038a6213a657b2260bc00d42814255
sjbeskur <sam.beskur@gmail.com>
2024-04-06 16:12:30 -0600

    updates

--- src/hamming.rs
+++ src/hamming.rs
@@ -1,3 +1,7 @@
+/// Hamming Distance measures the similarity between two strings of the same length.
+/// The Hamming Distance between two strings of the same length is the number of
+/// positions at which the corresponding characters are different. Since the length
+/// of these strings is equal, we can calculate the Hamming Distance.
 pub fn hamming_distance(bytes1: &[u8], bytes2: &[u8]) -> usize {
     let a: u32 = bytes1
         .iter()
@@ -32,4 +36,24 @@ fn test_hamming_distance() {
     let bytes1 = [0b00000000, 0b00000000, 0b00000000, 0b00000000];
     let bytes2 = [0b00000000, 0b00000000, 0b00000000, 0b00000101];
     assert_eq!(hamming_distance(&bytes1, &bytes2), 2);
+
+    let bytes1 = [0b00000000, 0b00000000, 0b00000000, 0b00000000];
+    let bytes2 = [0b00000000, 0b00000000, 0b00000000, 0b11111111];
+    assert_eq!(hamming_distance(&bytes1, &bytes2), 8);
+
+    let bytes1 = [0b00000000, 0b00000000, 0b00000000, 0b00000000];
+    let bytes2 = [0b00000000, 0b11111111, 0b00000000, 0b11111111];
+    assert_eq!(hamming_distance(&bytes1, &bytes2), 16);
+
+    let bytes1 = [0b00000000, 0b00000000, 0b00000000, 0b00000000];
+    let bytes2 = [0b00000000, 0b11111111, 0b11111111, 0b11111111];
+    assert_eq!(hamming_distance(&bytes1, &bytes2), 24);
+
+    let bytes1 = [0b00000000, 0b00000000, 0b00000000, 0b00000000];
+    let bytes2 = [0b11111111, 0b11111111, 0b11111111, 0b11111111];
+    assert_eq!(hamming_distance(&bytes1, &bytes2), 32);
+
+    let bytes1 = [0b00000000, 0b11111111, 0b00000000, 0b11111111];
+    let bytes2 = [0b11111111, 0b00000000, 0b11111111, 0b00000000];
+    assert_eq!(hamming_distance(&bytes1, &bytes2), 32);
 }
--- src/rand.rs
+++ src/rand.rs
@@ -3,51 +3,51 @@ use std::ops::RangeInclusive;
 pub struct Rand(u64);
 
 impl Rand {
-	pub fn new_with_seed(seed: u64) -> Self {
-		Self(seed)
-	}
+    pub fn new_with_seed(seed: u64) -> Self {
+        Self(seed)
+    }
 
-	pub fn next(&mut self) -> u64 {
-		self.0 = self.0.wrapping_mul(0x6C078965).wrapping_add(1);
-		self.0
-	}
+    pub fn next(&mut self) -> u64 {
+        self.0 = self.0.wrapping_mul(0x6C078965).wrapping_add(1);
+        self.0
+    }
 
-	pub fn next_max(&mut self, max_to_stay_below: usize) -> usize {
-		let max = max_to_stay_below as u64;
-		let rand = self.next() % max;
-		rand as usize
-	}
+    pub fn next_max(&mut self, max_to_stay_below: usize) -> usize {
+        let max = max_to_stay_below as u64;
+        let rand = self.next() % max;
+        rand as usize
+    }
 
-	pub fn gen_range(&mut self, range: RangeInclusive<f32>) -> f32 {
-		let min = *range.start();
-		let max = *range.end();
-		let range = max - min;
-		let rand = self.next() as f32 / u64::MAX as f32;
-		min + rand * range
-	}
+    pub fn gen_range(&mut self, range: RangeInclusive<f32>) -> f32 {
+        let min = *range.start();
+        let max = *range.end();
+        let range = max - min;
+        let rand = self.next() as f32 / u64::MAX as f32;
+        min + rand * range
+    }
 }
 
 // implement choose_multiple for Vec
 
 pub trait ChooseMultiple {
-	fn choose_multiple(&self, rng: &mut Rand, n: usize) -> Self
-	where
-		Self: Sized;
+    fn choose_multiple(&self, rng: &mut Rand, n: usize) -> Self
+    where
+        Self: Sized;
 }
 
 impl<T> ChooseMultiple for Vec<T>
 where
-	T: Clone,
+    T: Clone,
 {
-	fn choose_multiple(&self, rng: &mut Rand, n: usize) -> Self {
-		// choose multiple and don't repeat
-		let mut chosen = Vec::with_capacity(n);
-		let mut indices = (0..self.len()).collect::<Vec<_>>();
-		for _ in 0..n {
-			let index = rng.next_max(indices.len());
-			let index = indices.remove(index);
-			chosen.push(self[index].clone());
-		}
-		chosen
-	}
+    fn choose_multiple(&self, rng: &mut Rand, n: usize) -> Self {
+        // choose multiple and don't repeat
+        let mut chosen = Vec::with_capacity(n);
+        let mut indices = (0..self.len()).collect::<Vec<_>>();
+        for _ in 0..n {
+            let index = rng.next_max(indices.len());
+            let index = indices.remove(index);
+            chosen.push(self[index].clone());
+        }
+        chosen
+    }
 }
